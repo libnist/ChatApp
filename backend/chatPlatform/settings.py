05a2d3d91b5a83665c3ceccc81877dd8347bd5f1
libnist <ghahremani.mim@gmail.com>
2025-03-24 01:13:36 +0330

    add custom user model

--- backend/chatPlatform/settings.py
+++ backend/chatPlatform/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    "account",
+    "server",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "account.Account"
