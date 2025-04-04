cb7a7fc05f4cbfc72fc48dad1e0566175c191296
libnist <ghahremani.mim@gmail.com>
2025-04-04 15:28:01 +0330

    add swagger ui for api documentation

--- backend/chatPlatform/settings.py
+++ backend/chatPlatform/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    # 3rd party
+    "drf_spectacular",
+    
+    # local
     "account",
     "server",
 ]
@@ -130,3 +134,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Chat Platform API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+    # OTHER SETTINGS
+}
